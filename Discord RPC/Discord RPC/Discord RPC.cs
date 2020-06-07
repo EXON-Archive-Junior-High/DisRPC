472c63aebfc1833841248370e83b68d5fada5ab6
1-EXON <exon.indiv@gmail.com>
2020-06-07 20:10:57 +0900

    Upload

---
+++ Discord RPC/Discord RPC/Discord RPC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Discord_RPC
+{
+    public partial class Discord_RPC : Form
+    {
+        public Discord_RPC()
+        {
+            InitializeComponent();
+        }
+
+        private void Discord_RPC_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
