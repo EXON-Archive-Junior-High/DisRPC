a48a8c20f5dd46bd689a683c9769db4b6109eaa9
1-EXON <exon.indiv@gmail.com>
2020-06-07 20:56:24 +0900

    Upload

--- Discord RPC/Discord RPC/Properties/AssemblyInfo.cs
+++ Discord RPC/Discord RPC/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("8272d6c5-a558-4d2d-8b16-0b0486be0d37")]
+[assembly: Guid("cc07efff-d069-4757-8789-13fa7a5e8c13")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
